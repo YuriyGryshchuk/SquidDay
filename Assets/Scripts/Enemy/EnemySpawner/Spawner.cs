84ac352d5fb831ffc7311d245651346a4490014b
YuriyGryshchuk <yuriygrishchuk0@gmail.com>
2022-05-31 18:41:16 +0300

    Added Enemy Spawner and Destroer

--- Assets/Scripts/Enemy/EnemySpawner/Spawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner/Spawner.cs
@@ -4,6 +4,66 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _pull = new List<GameObject>();
+    [SerializeField] private Transform[] _spawnPositions;
+    [SerializeField] private int _startPullSize = 10;
+    [SerializeField] private GameObject _enemy;
 
+    private List<GameObject> _pullEnemy = new List<GameObject>();
+    private float _currentTime;
+    private int _previousSpawnPosition;
+    private int _currentSpawnPosition;
+
+    private void OnEnable()
+    {
+        CreatePullObject();
+        _currentTime = 0f;
+    }
+
+    private void Update()
+    {
+        SpawnDelay(3);
+    }
+
+    private void CreatePullObject()
+    {
+        for (int i = 0; i <= _startPullSize; i++)
+        {
+            _pullEnemy.Add(Instantiate(_enemy, transform.position, Quaternion.identity));
+        }
+
+        foreach (var enemy in _pullEnemy)
+        {
+            enemy.SetActive(false);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        foreach (var enemy in _pullEnemy)
+        {
+            if (!enemy.activeSelf)
+            {
+                enemy.SetActive(true);
+                enemy.transform.position = new Vector3(_spawnPositions[_currentSpawnPosition].transform.position.x,
+                    _spawnPositions[_currentSpawnPosition].transform.position.y, 0);
+                return;
+            }
+        }
+    }
+
+    protected void SpawnDelay(float timeWithSpawned)
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= timeWithSpawned)
+        {
+            _currentSpawnPosition = Random.Range(0, _spawnPositions.Length - 1);
+
+            if (_currentSpawnPosition != _previousSpawnPosition)
+            {
+                _previousSpawnPosition = _currentSpawnPosition;
+                SpawnEnemy();
+                _currentTime = 0;
+            }
+        }
+    }
 }
