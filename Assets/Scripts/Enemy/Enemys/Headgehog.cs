d5d5fd7c1eb5bb4fa125286bb81c7192c66086e7
Renat <qqz111iii@gmail.com>
2022-11-07 12:14:22 +0500

    Hedgehog rotation

--- Assets/Scripts/Enemy/Enemys/Headgehog.cs
+++ Assets/Scripts/Enemy/Enemys/Headgehog.cs
@@ -6,15 +6,26 @@ public class Headgehog : Enemy
 {
     private GameObject _target;
     private float _speed;
+    [SerializeField]
+    private float _rotationSpeed = 5;
 
     private void Update()
     {
         MoveToTarget();
+        RotateToTarget();
     }
 
     private void MoveToTarget()
     {
         transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 vectorToTarget = _target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * _rotationSpeed);
     }
 
     public void Init(GameObject target, float speed)
